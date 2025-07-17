56ba3194f830bf778528da21180d583a5caffb6c
Emin Furkan Eren <141373138+eeminfurkan@users.noreply.github.com>
2025-07-17 15:43:11 +0300

    .net 9
    Update to .NET 9.0 and add ApplicationUser class
    
    Updated project files to target .NET 9.0 across all components.
    Package references for ASP.NET Core and Entity Framework Core libraries have been upgraded to version 9.0.7.
    Introduced a new `ApplicationUser` class extending `IdentityUser` with optional properties `Ad` and `Soyad`.

---
+++ DestekSistemi.Entities/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DestekSistemi.Entities
+{
+    // IdentityUser'dan kalıtım alarak onu genişletiyoruz.
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(50)]
+        public string? Ad { get; set; } // Soru işareti, bu alanın boş olabileceğini belirtir.
+
+        [StringLength(50)]
+        public string? Soyad { get; set; }
+    }
+}
